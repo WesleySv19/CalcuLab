463a45cc12050626e1c7167f5410e9d18df8aeac
wesley_da_silva <wesleyysilvaa1912@gmail.com>
2025-01-16 00:55:19 -0300

    Calling the main functions of the calculator

--- src/components/Calculator/index.jsx
+++ src/components/Calculator/index.jsx
@@ -1,35 +1,40 @@
 import styles from './Calculator.module.css'
+import useCalculator from '../../hooks/useCalculator'
 
 const Calculator = () => {
+
+  const { displayValue, handleDigit, handleOperator, handleDecimal, handleEqual, clearDisplay } = useCalculator()
+
+
   return (
     <div className={styles.calc_container}>
 
       <div className={styles.calculator}>
 
-        <input className={styles.result} type="text" value='0' disabled />
+        <input className={styles.result} type="text" value={displayValue} disabled />
 
         <div className={styles.btn_container}>
-          <button className={styles.btns}>7</button>
-          <button className={styles.btns}>8</button>
-          <button className={styles.btns}>9</button>
-          <button className={`${styles.btns} ${styles.orangeColor}`}>/</button>
+          <button onClick={() => handleDigit('7')} className={styles.btns}>7</button>
+          <button onClick={() => handleDigit('8')} className={styles.btns}>8</button>
+          <button onClick={() => handleDigit('9')} className={styles.btns}>9</button>
+          <button onClick={() => handleOperator('/')} className={`${styles.btns} ${styles.orangeColor}`}>/</button>
 
-          <button className={styles.btns}>4</button>
-          <button className={styles.btns}>5</button>
-          <button className={styles.btns}>6</button>
-          <button className={`${styles.btns} ${styles.orangeColor}`}>*</button>
+          <button onClick={() => handleDigit('4')} className={styles.btns}>4</button>
+          <button onClick={() => handleDigit('5')} className={styles.btns}>5</button>
+          <button onClick={() => handleDigit('6')} className={styles.btns}>6</button>
+          <button onClick={() => handleOperator('*')} className={`${styles.btns} ${styles.orangeColor}`}>*</button>
 
-          <button className={styles.btns}>1</button>
-          <button className={styles.btns}>2</button>
-          <button className={styles.btns}>3</button>
-          <button className={`${styles.btns} ${styles.orangeColor}`}>-</button>
+          <button onClick={() => handleDigit('1')} className={styles.btns}>1</button>
+          <button onClick={() => handleDigit('2')} className={styles.btns}>2</button>
+          <button onClick={() => handleDigit('3')} className={styles.btns}>3</button>
+          <button onClick={() => handleOperator('-')} className={`${styles.btns} ${styles.orangeColor}`}>-</button>
 
           <button className={styles.btns}>0</button>
-          <button className={styles.btns}>.</button>
-          <button className={styles.btns}>C</button>
-          <button className={`${styles.btns} ${styles.orangeColor}`}>+</button>
+          <button onClick={() => handleDecimal('.')} className={styles.btns}>.</button>
+          <button onClick={() => clearDisplay()} className={styles.btns}>C</button>
+          <button onClick={() => handleOperator('+')} className={`${styles.btns} ${styles.orangeColor}`}>+</button>
 
-          <button className={styles.btns}>=</button>
+          <button onClick={() => handleEqual()} className={styles.btns}>=</button>
         </div>
       </div>
     </div>
